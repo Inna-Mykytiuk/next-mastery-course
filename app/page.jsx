aa15ac33525e14396ac223ee401ecffd6840305e
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-17 18:10:49 +0300

    upd

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
-        Discover & Share
+        Discover & Share 
         <br className='max-md:hidden' />
         <span className='blue_gradient'>AI-Powered Prompts</span>
       </h1>
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -82,7 +82,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
             Edit
           </p>
           <p
-            className='font-inter text-sm blue_gradient cursor-pointer'
+            className='font-inter text-sm text-red cursor-pointer'
             onClick={handleDelete}
           >
             Delete
