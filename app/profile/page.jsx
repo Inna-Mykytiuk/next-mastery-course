ef53ee668e8ff9e593dcb51c58f315fdc4c1d62e
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-17 16:09:30 +0300

    add posts

---
+++ app/profile/page.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+// import Profile from "@components/Profile";
+
+const Profile = () => {
+  return (
+    <div>
+      Profile
+    </div>
+  )
+}
+
+export default Profile
--- models/user.js
+++ models/user.js
@@ -22,4 +22,4 @@ export default User;
 // match: [
 //   /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
 //   "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
-// ],
+// ]
